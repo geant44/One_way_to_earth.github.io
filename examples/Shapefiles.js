7e12094f678ba39bab7bb39f53230900abce4882
HP <rtlab@cgu.edu.tw>
2019-10-16 17:29:27 +0800

    move file and add Average Surface Temp

--- examples/Shapefiles.js
+++ examples/Shapefiles.js
@@ -118,6 +118,42 @@ requirejs(['./WorldWindShim',
         fortStoryShapefile.load(null, null, fortStoryLayer);
         wwd.addLayer(fortStoryLayer);
 
+        for (var l = 0; l < layers.length; l++) {
+            layers[l].layer.enabled = layers[l].enabled;
+            wwd.addLayer(layers[l].layer);
+        }
+
+        //Average Surface Temp
+        // Web Map Service information from NASA's Near Earth Observations WMS
+        var serviceAddress = "https://neo.sci.gsfc.nasa.gov/wms/wms?SERVICE=WMS&REQUEST=GetCapabilities&VERSION=1.3.0";
+        // Named layer displaying Average Temperature data
+        var layerName = "MOD_LSTD_CLIM_M";
+
+        // Called asynchronously to parse and create the WMS layer
+        var createLayer = function (xmlDom) {
+            // Create a WmsCapabilities object from the XML DOM
+            var wms = new WorldWind.WmsCapabilities(xmlDom);
+            // Retrieve a WmsLayerCapabilities object by the desired layer name
+            var wmsLayerCapabilities = wms.getNamedLayer(layerName);
+            // Form a configuration object from the WmsLayerCapability object
+            var wmsConfig = WorldWind.WmsLayer.formLayerConfiguration(wmsLayerCapabilities);
+            // Modify the configuration objects title property to a more user friendly title
+            wmsConfig.title = "Average Surface Temp";
+            // Create the WMS Layer from the configuration object
+            var wmsLayer = new WorldWind.WmsLayer(wmsConfig);
+
+            // Add the layers to WorldWind and update the layer manager
+            wwd.addLayer(wmsLayer);
+            layerManager.synchronizeLayerList();
+        };
+
+        // Called if an error occurs during WMS Capabilities document retrieval
+        var logError = function (jqXhr, text, exception) {
+            console.log("There was a failure retrieving the capabilities document: " + text + " exception: " + exception);
+        };
+
+        $.get(serviceAddress).done(createLayer).fail(logError);
+		
         // Create a layer manager for controlling layer visibility.
         var layerManager = new LayerManager(wwd);
     });
